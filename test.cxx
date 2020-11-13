c71452f5d5be48d09b2bd3c10155fe8b9bbe030e
Fabian Pliquett <fpliquett@ikf.uni-frankfurt.de>
2020-11-13 19:09:47 +0100

    Still playing around...

--- test.cxx
+++ test.cxx
@@ -4,7 +4,7 @@ using namespace std;
 
 int main (int argc, char** argv)
 {
-	if (argc > 1) return 1;
-	printf("%s says \'Hello World!\'.\n", argv[0]);
+	if (argc > 2) return 1;
+	printf("%s says \'Hello World!\'.\n", argv[1]);
 	return 0;
 }
