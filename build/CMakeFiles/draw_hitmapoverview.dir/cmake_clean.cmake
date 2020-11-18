a02d728fb56839de6ccb0b8ba7fb24a6856b46f8
Fabian Pliquett <fpliquett@ikf.uni-frankfurt.de>
2020-11-18 10:12:50 +0100

    Added cmake files and compilation products in build folder.

---
+++ build/CMakeFiles/draw_hitmapoverview.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/draw_hitmapoverview.dir/src/macros/draw_hitmapoverview.cxx.o"
+  "draw_hitmapoverview"
+  "draw_hitmapoverview.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/draw_hitmapoverview.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/produce_all.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/produce_all.dir/src/macros/produce_all.cxx.o"
+  "produce_all"
+  "produce_all.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/produce_all.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
